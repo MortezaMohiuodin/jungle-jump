11adff8fd949528e967ecf4d068cb55b2cb83ba4
mortezamohiuodin <mortezamohiuodin@gmail.com>
2024-12-23 19:47:47 +0330

    add health to character

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+signal life_changed
+signal died
+
 @export var gravity = 750
 @export var run_speed = 150
 @export var jump_speed = 300
@@ -7,6 +10,7 @@ extends CharacterBody2D
 enum { IDLE , RUN , JUMP , HURT , DEAD}
 
 var state = IDLE
+var life = 3: set = set_life
 
 func _ready():
 	change_state(IDLE)
@@ -20,12 +24,21 @@ func change_state(new_state):
 			$AnimationPlayer.play('run')
 		HURT:
 			$AnimationPlayer.play('hurt')
+			velocity.y = -200
+			velocity.x = -100 * sign(velocity.x)
+			life -= 1
+			await get_tree().create_timer(.5).timeout
+			change_state(IDLE)
 		JUMP:
 			$AnimationPlayer.play('jump_up')
 		DEAD:
+			died.emit()
 			hide()
 
 func get_input():
+	if state == HURT:
+		return
+	
 	var right = Input.is_action_pressed("right")
 	var left = Input.is_action_pressed("left")
 	var jump = Input.is_action_just_pressed("jump")
@@ -65,4 +78,13 @@ func reset(_position):
 	position = _position
 	show()
 	change_state(IDLE)
+	life = 3
 	
+func set_life(value):
+	life = value
+	life_changed.emit(life)
+	if life <= 0:
+		change_state(DEAD)
+func hurt():
+	if state != HURT:
+		change_state(HURT)
